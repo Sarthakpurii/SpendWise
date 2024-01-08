271652df1a301e7f0969587853688276fa451210
Sarthak <sarthak.00puri@gmail.com>
2024-01-08 10:42:25 +0530

    updated dialogbox

--- lib/widget/dialog_box.dart
+++ lib/widget/dialog_box.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DialogPage extends StatefulWidget{
   DialogPage({super.key});
@@ -12,9 +13,17 @@ class DialogPage extends StatefulWidget{
 
 class _DialogPageState extends State<DialogPage>{
   final _titleController=TextEditingController();
+  final _priceController=TextEditingController();
+  DateTime? _selectedDate;
 
+  void _dateSelector() async {
+    final now=DateTime.now();
+    var selectedDate=await showDatePicker(context: context, firstDate: DateTime(now.year-1,now.month,now.day), lastDate: now,currentDate: now);
+    _selectedDate=selectedDate;
+  }
   @override
   void dispose(){
+    _priceController.dispose();
     _titleController.dispose();
     super.dispose();
   }
@@ -26,7 +35,19 @@ class _DialogPageState extends State<DialogPage>{
       decoration: const InputDecoration(label: Text('Title')),
       controller: _titleController,),
       Row(children: [
-        ElevatedButton(onPressed: (){print(_titleController.text);}, child: Text('Submit'))
+        Expanded(
+          child: TextField(keyboardType: TextInputType.number,
+                controller: _priceController,
+                decoration: const InputDecoration(label: Text('Amount')),),
+        ),
+      Expanded(child: Row(children: [
+        const Text('Selected Date'),
+        IconButton(onPressed: _dateSelector, icon: const Icon(Icons.calendar_month))
+      ],))
+      ],),
+      Row(children: [
+        ElevatedButton(onPressed: (){Navigator.pop(context);}, child: const Text('Cancel')),
+        ElevatedButton(onPressed: (){print(DateFormat('dd-MM-yyyy').format(_selectedDate!)); }, child: const Text('Submit'))
       ],)
     ]),);
   }
